c124fe938bbaf8606afdcb5be7aa934c8af0ea91
Ethan Martin <kxrider85@tamu.edu>
2021-12-20 03:19:37 -0600

    login/signup frontend working

--- backend/website/auth.py
+++ backend/website/auth.py
@@ -1,19 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 # same idea as views, but auth separate
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    data = request.form
+    
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
-    return '<p>Sign Up</p>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        #check if info is valid
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='e')
+        elif len(firstName) < 2:
+            flash('First name must be at least 2 characters', category='e')
+        elif password1 != password2:
+            flash('Passwords do not match', category='e')
+        elif password1 < 7:
+            flash('Password too short', category='e')
+        else:
+            # add user to database
+            flash('Account created!', category='s')
+            
+    return render_template('sign_up.html')
 
 
