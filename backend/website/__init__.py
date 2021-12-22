ff7a7847ce5f7a773898752d267988586c2d3406
kxrider <ethan.t.martin@outlook.com>
2021-12-22 15:19:18 -0600

    emailing functional

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -13,13 +13,13 @@ def create_app():
     app.config['SECRET_KEY'] = 'aoiejwg oaiewg'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     
-    app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USERNAME'] = 'e5583a58eacff3'
-    app.config['MAIL_PASSWORD'] = 'e4ab99ace109cb'
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_DEFAULT_SENDER'] = 'johndoe@hilbertserver.com'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'hilbert.host@gmail.com'
+    app.config['MAIL_PASSWORD'] = ''
+    #app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_DEFAULT_SENDER'] = 'hilbert.host@gmail.com'
     app.config['MAIL_DEBUG'] = True
     app.config['MAIL_SUPPRESS_SEND'] = False
     db.init_app(app)
